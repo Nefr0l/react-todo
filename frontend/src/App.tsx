8ab7668b8dbd0c818f5884ea91832f45e2dfa77e
Nefr0l <filip.wrzosek07@gmail.com>
2025-09-10 17:45:01 +0200

    feat: workin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import TaskList from './TaskList'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <p>Hello World</p>
-        
+      <p>Hello Worldd</p>
+      <TaskList></TaskList>
     </>
   )
 }
