81b2931c37dee0f8ccc9f74f91e549a78d048a73
Nefr0l <filip.wrzosek07@gmail.com>
2025-09-13 12:24:44 +0200

    feat: start change to sqlite

--- backend/main.go
+++ backend/main.go
@@ -3,40 +3,14 @@ package main
 import (
 	"database/sql"
 	"fmt"
-
-	"github.com/go-sql-driver/mysql"
 )
 
-var db *sql.DB
-
 func main() {
-	var err error
-
-	cfg := mysql.NewConfig()
-	cfg.User = "root"
-	cfg.Passwd = "admin"
-	cfg.Net = "tcp"
-	cfg.Addr = "localhost:3307"
-	cfg.DBName = "database"
-
-	db, err := sql.Open("mysql", cfg.FormatDSN())
+	db, err := sql.Open("sqlite3", "./db/app.sqlite")
 
 	handleErr(err)
-	fmt.Println("connected to the database")
 
-	// test query
-	rows, err := db.Query("select id, name from tasks;")
-	handleErr(err)
-
-	for rows.Next() {
-		var id int
-		var name string
-
-		err = rows.Scan(&id, &name)
-
-		handleErr(err)
-		fmt.Println(id, name)
-	}
+	defer db.Close()
 
 }
 
