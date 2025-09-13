bfb471047b30e75eed103c3168ab78b51a4f9446
Nefr0l <filip.wrzosek07@gmail.com>
2025-09-13 12:41:20 +0200

    almost working

--- backend/main.go
+++ backend/main.go
@@ -3,14 +3,32 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
+
+type task struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
 
 func main() {
 	db, err := sql.Open("sqlite3", "./db/app.sqlite")
-
 	handleErr(err)
 
 	defer db.Close()
+
+	http.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			rows, _ := db.Query("SELECT id, name FROM tasks")
+			defer rows.Close()
+		}
+	})
+
+	log.Println("Server running on :3001")
+	http.ListenAndServe(":3001", nil)
 
 }
 
